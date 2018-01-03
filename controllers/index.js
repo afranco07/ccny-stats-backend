8c676161a6ec0d44fa6214c1180207c7c6c1d63e
afranco07 <alberto.franco86@qmail.cuny.edu>
2018-01-03 14:39:26 -0500

    Added new route for getting/creating games

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/player', require('./player'));
 router.use('/team', require('./team'));
+router.use('/game', require('./game'));
 
 module.exports = router;
